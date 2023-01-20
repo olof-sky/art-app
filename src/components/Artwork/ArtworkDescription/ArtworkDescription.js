25b4a19f032f76e0e73ce632ab70e83be9580039
Sky <oloof92@gmail.com>
2023-01-20 15:14:09 +0100

    add artwork description

---
+++ src/components/Artwork/ArtworkDescription/ArtworkDescription.js
@@ -0,0 +1,22 @@
+function Description(props) {
+  const artwork = props.artwork;
+  return (
+    <section className="artwork_description">
+      <ul>
+        <li>Dating: {artwork.dating.presentingDate || "No dating listed"}</li>
+        <li>
+          Physical medium:{" "}
+          {artwork.physicalMedium || "No physical medium listed"}
+        </li>
+        <li>
+          Production place:{" "}
+          {artwork.productionPlaces.join(", ") || "No production place listed"}
+        </li>
+      </ul>
+      <p>Description:</p>
+      <p>{artwork.plaqueDescriptionEnglish || "No description available"}</p>
+    </section>
+  );
+}
+
+export default Description;
