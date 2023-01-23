77c0fa95cf548fca802685bb9928f999ff058bb8
Sky <oloof92@gmail.com>
2023-01-23 15:42:57 +0100

    add wiki links

---
+++ src/components/Artwork/artworkWikiLinks/ArtworkWikiLinks.js
@@ -0,0 +1,12 @@
+function ArtworkWikiLinks(props) {
+  const artist = props.artist;
+  const physicalMedium = props.physicalMedium;
+  return (
+    <span className="artwork_wiki_links">
+      <a href={artist.url}>More on artist: {artist.name}</a>
+      <br></br>
+      <a href={physicalMedium.url}>More on technique: {physicalMedium.name}</a>
+    </span>
+  );
+}
+export default ArtworkWikiLinks;
--- src/routes/ArtworkPage/ArtworkPage.jsx
+++ src/routes/ArtworkPage/ArtworkPage.jsx
@@ -4,6 +4,7 @@ import { useParams } from "react-router-dom";
 import BtnRoundLink from "../../components/buttons/BtnRoundLink";
 import Artwork from "../../components/artwork/Artwork";
 import ArtworkDescription from "../../components/artwork/artworkDescription/ArtworkDescription";
+import ArtworkWikiLinks from "../../components/artwork/artworkWikiLinks/ArtworkWikiLinks";
 
 const key = process.env.REACT_APP_MASTER_KEY;
 const url = process.env.REACT_APP_RIJKS_URL;
@@ -11,6 +12,8 @@ const baseUrl = process.env.REACT_APP_BASE_URL;
 
 function ArtworkPage() {
   const [artwork, setArtwork] = useState({});
+  const [artistId, setArtistId] = useState("");
+  const [physicalMediumId, setPhysicalMediumId] = useState("");
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(false);
   const { id } = useParams();
@@ -18,6 +21,40 @@ function ArtworkPage() {
   useEffect(() => {
     getArtworkById(id);
   }, []);
+
+  function setWikipediaPageId(query, type) {
+    var wikiUrl = "https://en.wikipedia.org/w/api.php";
+    var params = {
+      action: "query",
+      list: "search",
+      srsearch: query,
+      format: "json",
+    };
+
+    wikiUrl = wikiUrl + "?origin=*";
+    Object.keys(params).forEach(function (key) {
+      wikiUrl += "&" + key + "=" + params[key];
+    });
+    try {
+      fetch(wikiUrl)
+        .then((response) => {
+          return response.json();
+        })
+        .then((data) => {
+          if (type === "artist") {
+            setArtistId(data.query.search[0].pageid);
+          }
+          if (type === "physicalMedium") {
+            setPhysicalMediumId(data.query.search[0].pageid);
+          }
+          return data;
+        });
+    } catch (error) {
+      console.log(error);
+      setError(true);
+      throw new Error();
+    }
+  }
 
   async function getArtworkById(id) {
     setLoading(true);
@@ -28,6 +65,8 @@ function ArtworkPage() {
       } else {
         const data = await response.json();
         setArtwork(data.artObject);
+        setWikipediaPageId(data.artObject.principalOrFirstMaker, "artist");
+        setWikipediaPageId(data.artObject.physicalMedium, "physicalMedium");
         setLoading(false);
         console.log("Data::", data);
         return data;
@@ -46,12 +85,23 @@ function ArtworkPage() {
   if (loading) {
     return <div>Loading</div>;
   } else {
+    const artistUrl = `http://en.wikipedia.org/?curid=${artistId}`;
+    const physicalMediumUrl = `http://en.wikipedia.org/?curid=${physicalMediumId}`;
+    const artistWiki = { name: artwork.principalOrFirstMaker, url: artistUrl };
+    const physicalMediumWiki = {
+      name: artwork.physicalMedium,
+      url: physicalMediumUrl,
+    };
     return (
       <main className="artwork_container">
         <Artwork
           imgSrc={artwork.webImage.url}
           maker={artwork.principalOrFirstMaker}
           title={artwork.title}
+        />
+        <ArtworkWikiLinks
+          artist={artistWiki}
+          physicalMedium={physicalMediumWiki}
         />
         <ArtworkDescription artwork={artwork} />
         <BtnRoundLink link={baseUrl} goBack={true} />
