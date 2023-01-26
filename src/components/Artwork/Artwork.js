445b77871ed395d139186d8961c03a24eea35161
Sky <oloof92@gmail.com>
2023-01-26 14:15:49 +0100

    add className

--- src/components/Artwork/Artwork.js
+++ src/components/Artwork/Artwork.js
@@ -1,9 +1,16 @@
 import ArtworkLabel from "./artworkLabel/ArtworkLabel";
+import "../../assets/styles/components/artwork.scss";
 
 function Artwork(props) {
   return (
     <span className="artwork_image_and_label">
-      <img src={props.imgSrc} loading="lazy" alt={props.title} id={props.id} />
+      <img
+        className="artwork_image"
+        src={props.imgSrc}
+        loading="lazy"
+        alt={props.title}
+        id={props.id}
+      />
       <ArtworkLabel {...props} />
     </span>
   );
