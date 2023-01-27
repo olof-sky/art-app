c7b10823f264e5574e568514113674f9f3b8001a
Sky <oloof92@gmail.com>
2023-01-27 11:20:06 +0100

    refactor

--- src/components/Artwork/ArtworkLabel/ArtworkLabel.js
+++ src/components/Artwork/ArtworkLabel/ArtworkLabel.js
@@ -1,6 +1,7 @@
+import { React, useEffect } from "react";
+import { getContrastYIQ } from "../../../helpers/helpers";
 import BtnRoundLink from "../../buttons/BtnRoundLink";
 import "../../../assets/styles/components/artworkLabel.scss";
-import { React, useEffect } from "react";
 
 const baseUrl = process.env.REACT_APP_BASE_URL;
 
@@ -24,15 +25,6 @@ function Label(props) {
   function setTextColor(label, color) {
     const textColor = getContrastYIQ(color);
     return (label.style.color = textColor);
-  }
-
-  function getContrastYIQ(hexcolor) {
-    hexcolor = hexcolor.replace("#", "");
-    var r = parseInt(hexcolor.substr(0, 2), 16);
-    var g = parseInt(hexcolor.substr(2, 2), 16);
-    var b = parseInt(hexcolor.substr(4, 2), 16);
-    var yiq = (r * 299 + g * 587 + b * 114) / 1000;
-    return yiq >= 128 ? "black" : "white";
   }
 
   const link = `${baseUrl}/artwork/${props.id}`;
